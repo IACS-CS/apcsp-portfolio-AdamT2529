f656a726cf014338bde0b516be22c16c38cee344
AdamT2529 <profileytwow1@gmail.com>
2025-09-29 13:28:40 +0000

    Tried to add citation page

---
+++ src/scripts/pages/citations.js
@@ -0,0 +1,13 @@
+import { renderMenu } from "../utils/renderMenu";
+
+export function renderCitations () {
+  /* html */
+  return `
+  <h1>Citations</h1>
+  <p>Initial template from my teacher Mr. Hinkle</
+  p>
+  <p>Used Copilot to help with... </p>
+  <p>(later fill in libraries you use etc.)<p>
+  <p>(later fill in media citations)<p>
+  `;
+}
--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -5,7 +5,7 @@ export function renderWelcome() {
   /* html */
   return `
     <!-- code generated in welcome.js -->
-    <h1>Student Portfolio</h1>
+    <h1>Adam Tran's Portfolio</h1>
     ${renderMenu()}
     <h2>About Me</h2>
     <p>All About me...</p>
