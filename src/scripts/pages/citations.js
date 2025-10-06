cf840a54c6a98a4b8a200bdacb28ada76a4312a0
AdamT2529 <profileytwow1@gmail.com>
2025-10-06 13:29:04 +0000

    Added page links to the citations page

--- src/scripts/pages/citations.js
+++ src/scripts/pages/citations.js
@@ -1,13 +1,13 @@
 import { renderMenu } from "../utils/renderMenu";
 
-export function renderCitations () {
+export function renderCitations() {
   /* html */
   return `
-  <h1>Citations</h1>
-  <p>Initial template from my teacher Mr. Hinkle</
-  p>
-  <p>Used Copilot to help with... </p>
-  <p>(later fill in libraries you use etc.)<p>
-  <p>(later fill in media citations)<p>
+  <h1 data-aos="fade-up">Citations</h1>
+    ${renderMenu()}
+    <p>Initial template from my teacher Mr. Hinkle</p>
+    <p>Used Copilot to help with making dark and light mode button, citations page </p>
+    <p>(Used AOS - Animate On Scroll library)</p>
+    <p>(later fill in media citations)</p>
   `;
 }
