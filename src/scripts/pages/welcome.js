a7444c4e7d4827e68018207174c4fde50b4c9cc8
AdamT2529 <profileytwow1@gmail.com>
2025-10-10 18:38:52 +0000

    Project is completed replaced the circle image with a new one making it small and finished some final touches.

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -8,9 +8,9 @@ export function renderWelcome() {
     <h1 data-aos="fade-up">Adam's Portfolio</h1>
     ${renderMenu()}
     <h2 data-aos="zoom-in-up">About Me</h2>
-    <p data-aos="fade-in">I am a student at IACS. I am taking the class of AP Computer Science Principles and this website consists of my favorite work I created in this course.</p>
+    <p data-aos="fade-in">I am a student at IACS. I am taking the class of AP Computer Science Principles and this website consists of my favorite work I have created in this course.</p>
     <!-- an example image: Notice that images live in public/ -->
-    <img src="./example-circle.svg" alt="A blue circle" >
+    <img src="./IACS.jpeg" alt="A blue circle" class="small-image">
 
     <!-- end code generated in welcome.js -->
   `;
